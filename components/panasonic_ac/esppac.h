254af7d1ded466856e09eb10675eae47bb3c16b8
ladagal <53316434+ladagal@users.noreply.github.com>
2024-09-30 15:03:38 +0200

    Update esppac.h

--- components/panasonic_ac/esppac.h
+++ components/panasonic_ac/esppac.h
@@ -16,8 +16,8 @@ static const char *const VERSION = "2.0.0";
 static const uint8_t BUFFER_SIZE = 128;  // The maximum size of a single packet (both receive and transmit)
 static const uint8_t READ_TIMEOUT = 20;  // The maximum time to wait before considering a packet complete
 
-static const uint8_t MIN_TEMPERATURE = 16;     // Minimum temperature as reported by Panasonic app
-static const uint8_t MAX_TEMPERATURE = 30;     // Maximum temperature as supported by Panasonic app
+static const uint8_t MIN_TEMPERATURE = 10;     // Minimum temperature as reported by Panasonic app
+static const uint8_t MAX_TEMPERATURE = 35;     // Maximum temperature as supported by Panasonic app
 static const float TEMPERATURE_STEP = 0.5;     // Steps the temperature can be set in
 static const float TEMPERATURE_TOLERANCE = 2;  // The tolerance to allow when checking the climate state
 static const uint8_t TEMPERATURE_THRESHOLD =
